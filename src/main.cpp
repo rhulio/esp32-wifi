ff9f5fd84ab97c25c0c4a214702eba041374291a
rhulio <rhulio@2br.com.br>
2019-05-09 21:47:12 -0300

    esp32-wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,26 @@
+/*
+
+RScada v0.1a
+Projeto com a finalidade de medição de latência e nível de sinal de um Wi-Fi.
+Envio: MQTT
+Programação: Arduino
+Microcontrolador: ESP32
+
+*/
+
 #include <WiFi.h>
-#include <HTTPClient.h>
+//#include <HTTPClient.h>
 
 const char* ssid = "# Cipriano";
 const char* senha = "globo321";
 
-const String token = "438C1C";
+#include <MQTT.h>
+
+WiFiClient net;
+MQTTClient mqtt;
+
+//const String token = "438C1C";
+const char* token = "438C1C";
 
 String float2str(float x, byte precision = 2) {
   char tmp[50];
@@ -22,7 +38,6 @@ bool conectaWiFi() {
 }
 
 void wdt() {
-  ESP.wdtFeed();
   yield();
 }
 
@@ -37,8 +52,12 @@ void setup() {
   //WiFi.setSleep(false);
   
   conectaWiFi();
+    
+  mqtt.begin("mqtt.rscada.ga", net);
+  mqtt.connect(token, token, token);
 }
 
+/*
 String WiFiGET(String link) {
   if (!conectaWiFi()) return "";
 
@@ -58,6 +77,7 @@ String WiFiGET(String link) {
   http.end(); // Encerra conexão HTTP.
   return "";
 }
+*/
 
 unsigned long tempoTotal = 0;
   
@@ -67,18 +87,32 @@ void loop() {
 
     //String dadosGet;
     String sinal = String(WiFi.RSSI());
+    
+    /*
     String dadosGet = "";
 
     if(tempoTotal > 0)
       dadosGet = "sinal=" + sinal + "&latencia=" + String(tempoTotal);
     else
       dadosGet = "sinal=" + sinal;
-
-    
+        
     String webservice = WiFiGET("http://sistema.rscada.ga/api/"+token+"/envio?"+dadosGet);
+    */
 
-    tempoTotal = millis() - tempoInicial;
-    
+    if(mqtt.connected()){
+      mqtt.publish(String(token)+"/sinal", sinal, false, 1);
+      tempoTotal = millis() - tempoInicial;
+
+      if(tempoTotal > 0){
+        mqtt.publish(String(token)+"/latencia", String(tempoTotal), false, 1);
+        Serial.println("[MQTT] Informações enviadas ao servidor.\nLatência: "+String(tempoTotal)+" ms - Nível de Sinal: "+sinal+" dBm");
+      } else
+        Serial.println("[MQTT] Informações enviadas ao servidor.\nNível de Sinal: "+sinal+" dBm");
+    } else {
+      mqtt.disconnect();
+      mqtt.connect(token, token, token);
+    }
+
     while((millis() - tempoInicial) < 200) wdt();
   }
   wdt();
